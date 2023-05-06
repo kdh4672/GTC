ab9e292c5d38b809df7afe99f0d296363ac59fa7
daehyeon.kong <daehyeon.kong@navercorp.com>
2023-05-06 13:34:32 +0900

    extract embedding

---
+++ extract_embedding.sh
@@ -0,0 +1,10 @@
+# python extract_embedding.py --dataset cifar10 --is_test_dataset True
+# python extract_embedding.py --dataset cifar10 --is_test_dataset False
+# python extract_embedding.py --dataset stl10 --is_test_dataset True
+# python extract_embedding.py --dataset stl10 --is_test_dataset False
+# python extract_embedding.py --dataset imagenet_dog --is_test_dataset True
+# python extract_embedding.py --dataset imagenet_dog --is_test_dataset False
+python extract_embedding.py --dataset mit --is_test_dataset True
+# python extract_embedding.py --dataset mit --is_test_dataset False
+python extract_embedding.py --dataset stanford40 --is_test_dataset True
+# python extract_embedding.py --dataset stanford40 --is_test_dataset Fa
